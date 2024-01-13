1b087328f134015e11cccabe0e0baa9dab91eaac
bachidze <bachidze90@gmail.com>
2024-01-13 22:07:20 +0400

    second commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,47 +1,77 @@
-import React from 'react'
-import style from './Card.module.css'
+import React, { useState } from 'react';
+import style from './Card.module.css';
 
 const Card = ({
-            imgUrl ='error',
-            name ='error',
-            desc = 'error',
-            hover ='error',
-            button ='error',
-            number1 ='error',
-            number2 ='error',
-            number3 ='derror',
-            number4 ='error',
-            number5='error',
-            }) => {   
+  imgUrl = 'error',
+  name = 'error',
+  desc = 'error',
+  hover = 'error',
+  button = 'error',
+  number1 = 'error',
+  number2 = 'error',
+  number3 = 'error',
+  number4 = 'error',
+  number5 = 'error',
+}) => {
+  const [clickedNumber, setClickedNumber] = useState(null);
+
+  const handleNumberClick = (number) => {
+    setClickedNumber(number);
+  };
+
   return (
-
     <div className={style.carddiv}>
-
-        <div className={style.starround}>
-
+      <div className={style.starround}>
         <img src={imgUrl} alt="StarImg" />
+      </div>
 
+      <h1 className={style.title}>{name}</h1>
+
+      <p className={style.paragraph}>{desc}</p>
+
+      <div className={style.flex}>
+        <div
+          style={{ backgroundColor: clickedNumber === number1 ? 'orange' : '' }}
+          className={`${style.round} ${style[hover]}`}
+          onClick={() => handleNumberClick(number1)}
+        >
+          {number1}
         </div>
-
-        <h1 className={style.title}>{name}</h1>
-
-        <p className={style.paragraph}>{desc}</p>
-
-        <div className={style.flex}>
-
-        <div className={`${style.round} ${style[hover]}`}>{number1}</div>    
-        <div className={`${style.round} ${style[hover]}`}>{number2}</div>    
-        <div className={`${style.round} ${style[hover]}`}>{number3}</div>    
-        <div className={`${style.round} ${style[hover]}`}>{number4}</div>    
-        <div className={`${style.round} ${style[hover]}`}>{number5}</div> 
-
+        <div
+          style={{ backgroundColor: clickedNumber === number2 ? 'orange' : '' }}
+          className={`${style.round} ${style[hover]}`}
+          onClick={() => handleNumberClick(number2)}
+        >
+          {number2}
         </div>
-        <form className={style.form} >
+        <div
+          style={{ backgroundColor: clickedNumber === number3 ? 'orange' : '' }}
+          className={`${style.round} ${style[hover]}`}
+          onClick={() => handleNumberClick(number3)}
+        >
+          {number3}
+        </div>
+        <div
+          style={{ backgroundColor: clickedNumber === number4 ? 'orange' : '' }}
+          className={`${style.round} ${style[hover]}`}
+          onClick={() => handleNumberClick(number4)}
+        >
+          {number4}
+        </div>
+        <div
+          style={{ backgroundColor: clickedNumber === number5 ? 'orange' : '' }}
+          className={`${style.round} ${style[hover]}`}
+          onClick={() => handleNumberClick(number5)}
+        >
+          {number5}
+        </div>
+      </div>
+
+      <form className={style.form}>
         <button className={`${style.submit} ${style[hover]}`}>{button}</button>
-        </form>
-
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
